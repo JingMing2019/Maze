0007ef4f003de5bdf9712d904ea9c1e5e07c5fdd
Jing Ming <jingming@Jings-MacBook-Pro.local>
2022-05-06 12:50:32 -0700

    Define 4 direction using enum

---
+++ Direction.java
@@ -0,0 +1,10 @@
+/**
+ * The {@code Direction} class represents the 4 used direction in the maze cell and the possible move of the player.
+ */
+
+public enum Direction {
+    NORTH,
+    SOUTH,
+    EAST,
+    WEST,
+}
